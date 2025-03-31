913f60eaaf49df854b2b25deda54882e921f5609
jacek <jacekrak@jacek.local>
2025-03-31 19:05:52 +0200

    first

---
+++ zal2/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./AWWW/zal2/**/*.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
